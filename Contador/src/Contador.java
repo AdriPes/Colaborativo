79086ac9c2022232dd5046046a8d3fbb85e0eecf
Alejandro <alejandro.graapa@sanviatorvalladolid.com>
2024-04-18 10:46:33 +0200

    primero

--- Contador/src/Contador.java
+++ Contador/src/Contador.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
 
 public class Contador {
 	public static void main(String[]args) {
+		Scanner scan = new Scanner(System.in);
+		int opcion =0;
+		System.out.println("Que numero quiere elegir");
+		opcion= scan.nextInt();
+
 		for (int i = 0;i<1001;i++) {
+
 			System.out.println("Hola estoy contando hasta 1000 desde: " + i);
 		}
 		
