49c5c04393226028adae4d5497fb8a804b2c245c
AdriPes <adrian.pesont@sanviatorvalladolid.com>
2024-04-18 10:03:37 +0200

    Contador

---
+++ Contador/src/Contador.java
@@ -0,0 +1,10 @@
+
+public class Contador {
+	public static void main(String[]args) {
+		for (int i = 0;i<1001;i++) {
+			System.out.println("Hola estoy contando hasta 1000 desde: " + i);
+		}
+		
+		
+	}
+}
